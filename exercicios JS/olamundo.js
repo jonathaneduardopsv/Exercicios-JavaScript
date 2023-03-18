234557853904dc71aa8e0bbb17c17181b8104bfd
jonathaneduardopsv <jonathanvale92@gmail.com>
2023-03-17 21:27:25 -0300

    ...

---
+++ exercicios JS/olamundo.js
@@ -0,0 +1,10 @@
+var texto = "Hello World";
+
+function imprimirTexto(textoParaImprimir) {
+   console.log(textoParaImprimir);
+}
+
+imprimirTexto(texto);
+var idade = 18 
+var atendeAClassificacaoEtaria = idade >= 18 ||
+console.log (atendeAClassificacaoEtaria)
